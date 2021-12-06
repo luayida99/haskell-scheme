62067d49e192549c94dd0a7ca4e59b715dd86357
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-12-06 19:13:50 +0800

    Add REPL

--- ch3/numericeval.hs
+++ ch3/numericeval.hs
@@ -7,18 +7,42 @@ import Control.Monad
 import System.Environment 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad.Error
+import System.IO hiding (try)
 
 main :: IO ()
 main = do 
     args <- getArgs 
-    evaluated <- return $ liftM show $ readExpr (args !! 0) >>= eval 
-    putStrLn $ extractValue $ trapError evaluated
+    case length args of 
+        0 -> runREPL
+        1 -> evalAndPrint $ args !! 0 
+        otherwise -> putStrLn ("Program only takes 0 or 1 argument.")
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
 spaces :: Parser () 
 spaces = skipMany1 space 
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine 
+
+evalString :: String -> IO String 
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: (Monad m) => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do 
+                                result <- prompt 
+                                if pred result then return () 
+                                else action result >> until_ pred prompt action 
+
+runREPL :: IO ()
+runREPL = until_ (=="quit") (readPrompt "YDScheme>>> ") evalAndPrint
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
